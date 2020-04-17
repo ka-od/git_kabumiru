2d25cf9fdf6feafbd08fddcd9f38842d6828d21f
EC2 Default User <ec2-user@ip-172-31-41-157.ap-northeast-1.compute.internal>
2020-04-17 19:58:37 +0000

    売上高営業利益率のインスタンスメソッドを定義

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,11 @@ class Company < ApplicationRecord
 
 
   def haitou_rimawari
-    return 100 * haitou / (kabuka * 100)
+    return 100 * haitou / (kabuka * 100)  #配当利回りの計算
+  end
+
+  def eiri_ritu
+    return 100 * eigyou_rieki / uriagedaka  #売上高営業利益率の計算
   end
 
 end
