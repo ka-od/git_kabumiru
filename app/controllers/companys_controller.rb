cb134c56934d0f22b7744a68d6c97ebcdd8a9d06
EC2 Default User <ec2-user@ip-172-31-41-157.ap-northeast-1.compute.internal>
2020-04-17 20:04:23 +0000

    売上高営業利益率をcompanyコントローラーでインスタンスメソッドを使用して計算

--- app/controllers/companys_controller.rb
+++ app/controllers/companys_controller.rb
@@ -7,6 +7,7 @@ class CompanysController < ApplicationController
   def show
     @company = Company.find(params[:id])  #companyクラスから調べたいidに該当する銘柄情報をデータベースから取得。
     @haitou_rimawari = @company.haitou_rimawari #@companyというインスタンスにhaitou_rimawariというメソッドを使用。
+    @eiri_ritu = @company.eiri_ritu #@companyというインスタンスにeiri_rituというメソッドを使用。
   end
 
 
