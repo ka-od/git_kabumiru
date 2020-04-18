2ef59bd02affaa7c31a2345fd42b5ffd4aa5ada7
EC2 Default User <ec2-user@ip-172-31-41-157.ap-northeast-1.compute.internal>
2020-04-18 20:25:51 +0000

    scraping.rbでデータベースの全銘柄情報を更新できるように変更

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -4,17 +4,17 @@ class Scraping
   def self.stock_info
     agent = Mechanize.new
     
-    num = 3401
-    while num < 3403 do
-      page = agent.get("https://minkabu.jp/stock/#{num}/settlement")
+    @companys = Company.all  #companyクラスの銘柄情報をデータベースから取得。
+    @companys.each do |com|
+      puts com
+      page = agent.get("https://minkabu.jp/stock/#{com.id}/settlement")
       name = page.search('.md_stockBoard_stockName').inner_text
       uriagedaka = page.at('.ly_content_wrapper.size_ss .num.vamd').inner_text
     
-      company = Company.where(id: num).first_or_initialize
+      company = Company.where(id: com.id).first_or_initialize
       company.name = name
       company.uriagedaka = uriagedaka
       company.save
-      num = num + 1
     end
   end
 
