0c8afb92db95be6fc8daeb7b1ca6a0c55217fb24
EC2 Default User <ec2-user@ip-172-31-41-157.ap-northeast-1.compute.internal>
2020-04-18 06:01:16 +0000

    searchアクションのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root  'companys#index'                        #ルートパスの指定
   
-  resources :companys, only: [:show, :index]
-  
+  resources :companys, only: [:show, :index] do
+    collection do
+      get 'search'
+    end
+  end
 end
