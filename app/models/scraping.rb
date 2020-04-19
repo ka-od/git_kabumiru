f6e8165dcb4e20d989e45ed05af27d09230fde2e
EC2 Default User <ec2-user@ip-172-31-41-157.ap-northeast-1.compute.internal>
2020-04-19 00:56:03 +0000

    銘柄スクレイピングのエラー回避。上場廃止銘柄などをスキップ

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,21 +1,32 @@
 class Scraping
 #require 'mechanize'
 
+  #全銘柄情報をスクレイピングするメソッド
   def self.stock_info
     agent = Mechanize.new
     
-    @companys = Company.all  #companyクラスの銘柄情報をデータベースから取得。
+    #companyクラスの全銘柄情報をデータベースから取得。
+    @companys = Company.all
+
+    #各銘柄情報をスクレイピング
     @companys.each do |com|
-      puts com
-      page = agent.get("https://minkabu.jp/stock/#{com.id}/settlement")
-      name = page.search('.md_stockBoard_stockName').inner_text
-      uriagedaka = page.at('.ly_content_wrapper.size_ss .num.vamd').inner_text
+      meigara_page = agent.get("https://minkabu.jp/stock/#{com.id}")  #銘柄個別ページを読み込み
+      elements = meigara_page.search('.md_ntab_stock .md_notice_mini.dpib')
+      if elements.inner_text.include?("決算")         #決算情報のページ有無確認して、上場廃止銘柄などでのエラー停止を回避
+        page = agent.get("https://minkabu.jp/stock/#{com.id}/settlement")
+      
+        name = page.at('.md_stockBoard_stockName').inner_text   if page.at('.md_stockBoard_stockName')
+        uriagedaka = page.at('.ly_content_wrapper.size_ss .num.vamd').inner_text  if page.at('.ly_content_wrapper.size_ss .num.vamd')
     
-      company = Company.where(id: com.id).first_or_initialize
-      company.name = name
-      company.uriagedaka = uriagedaka
-      company.save
-    end
-  end
+        company = Company.where(id: com.id).first_or_initialize
+        company.name = name
+        company.uriagedaka = uriagedaka
+        company.save
+      else
+        next
+      end
+    end   # "@companys.each" ここまで
+  end   # "self.stock_info"　ここまで
+
 
 end
