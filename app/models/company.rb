cb20575318a10717d2266401cc35061b208a4d07
EC2 Default User <ec2-user@ip-172-31-41-157.ap-northeast-1.compute.internal>
2020-04-19 02:44:06 +0000

    優待利回りメソッド定義作成

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,10 +2,15 @@ class Company < ApplicationRecord
 
   #メソッド：配当利回り
   def haitou_rimawari
-    return 100 * haitou / (kabuka * 100)  if kabuka.present? && haitou.present?
+    return 100 * haitou / (kabuka * 100)  if haitou.present? && kabuka.present?
   end
 
-  #メソッド：売上高営業利益率の計算
+  #メソッド：優待利回り
+  def yuutai_rimawari
+    return 100 * yuutai / (kabuka * 100)  if yuutai.present? && kabuka.present?
+  end
+
+  #メソッド：売上高営業利益率
   def eiri_ritu
     return 100 * eigyou_rieki / uriagedaka  if eigyou_rieki.present? && uriagedaka.present?
   end
