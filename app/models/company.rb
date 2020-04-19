9f2643d545e161251674c777a51b2b6a99422860
EC2 Default User <ec2-user@ip-172-31-41-157.ap-northeast-1.compute.internal>
2020-04-19 02:38:09 +0000

    データがnullの場合演算をスキップしてエラー回避

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,12 +1,13 @@
 class Company < ApplicationRecord
 
-
+  #メソッド：配当利回り
   def haitou_rimawari
-    return 100 * haitou / (kabuka * 100)  #配当利回りの計算
+    return 100 * haitou / (kabuka * 100)  if kabuka.present? && haitou.present?
   end
 
+  #メソッド：売上高営業利益率の計算
   def eiri_ritu
-    return 100 * eigyou_rieki / uriagedaka  #売上高営業利益率の計算
+    return 100 * eigyou_rieki / uriagedaka  if eigyou_rieki.present? && uriagedaka.present?
   end
 
 end
