bc017ac8d1438eda2d79c77e3a0a0a68edb66c71
EC2 Default User <ec2-user@ip-172-31-41-157.ap-northeast-1.compute.internal>
2020-04-25 21:45:51 +0000

    優待株判定メソッドを修正

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,27 +2,28 @@ class Company < ApplicationRecord
 
   #メソッド：売上高営業利益率
   def eiri_ritu
-    return  100 * eigyou_rieki / uriagedaka  if eigyou_rieki.present? && uriagedaka.present?
+    return  100 * eigyou_rieki / uriagedaka                 if eigyou_rieki.present? && uriagedaka.present?
   end
 
 
   #メソッド：合計利回り
   def goukei_rimawari
-    return  haitou_rimawari + yutai_rimawari    if haitou_rimawari.present? && yutai_rimawari.present?
+    return  haitou_rimawari + yutai_rimawari                if haitou_rimawari.present? && yutai_rimawari.present?
   end
   
   
   #メソッド：割安度
   def wariyasudo
-    return  (1- kabuka / riron_kabuka) * 100                 if kabuka.present? && riron_kabuka.present?
+    return  (1- kabuka / riron_kabuka) * 100                if kabuka.present? && riron_kabuka.present?
   end
 
 
   #メソッド：優待株オススメ判定
   def yutai_hantei
-    goukei_rimawari = haitou_rimawari + yutai_rimawari    if haitou_rimawari.present? && yutai_rimawari.present?
-    wariyasudo = 1- kabuka / riron_kabuka                 if kabuka.present? && riron_kabuka.present?
-    if goukei_rimawari >= 4 && wariyasudo >= 25
+    goukei_rimawari = haitou_rimawari + yutai_rimawari      if haitou_rimawari.present? && yutai_rimawari.present?
+    wariyasudo = (1- kabuka / riron_kabuka) * 100           if kabuka.present? && riron_kabuka.present?
+    if goukei_rimawari >= 3 && wariyasudo >= 25
+      return  true
     end
   end
 
