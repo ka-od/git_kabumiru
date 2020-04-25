c46578aa5e690a881fbda59ea2943abf4c7a8fb8
EC2 Default User <ec2-user@ip-172-31-41-157.ap-northeast-1.compute.internal>
2020-04-25 20:15:41 +0000

    メソッド作成：割安度, 合計利回り、優待株判定

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,18 +1,30 @@
 class Company < ApplicationRecord
 
-  #メソッド：配当利回り
-  def haitou_rimawari
-    return 100 * haitou / (kabuka * 100)  if haitou.present? && kabuka.present?
-  end
-
-  #メソッド：優待利回り
-  def yuutai_rimawari
-    return 100 * yuutai / (kabuka * 100)  if yuutai.present? && kabuka.present?
-  end
-
   #メソッド：売上高営業利益率
   def eiri_ritu
-    return 100 * eigyou_rieki / uriagedaka  if eigyou_rieki.present? && uriagedaka.present?
+    return  100 * eigyou_rieki / uriagedaka  if eigyou_rieki.present? && uriagedaka.present?
   end
+
+
+  #メソッド：合計利回り
+  def goukei_rimawari
+    return  haitou_rimawari + yutai_rimawari    if haitou_rimawari.present? && yutai_rimawari.present?
+  end
+  
+  
+  #メソッド：割安度
+  def wariyasudo
+    return  1- kabuka / riron_kabuka                 if kabuka.present? && riron_kabuka.present?
+  end
+
+
+  #メソッド：優待株オススメ判定
+  def yutai_hantei
+    goukei_rimawari = haitou_rimawari + yutai_rimawari    if haitou_rimawari.present? && yutai_rimawari.present?
+    wariyasudo = 1- kabuka / riron_kabuka                 if kabuka.present? && riron_kabuka.present?
+    if goukei_rimawari >= 4 && wariyasudo >= 0.2
+    end
+  end
+
 
 end
