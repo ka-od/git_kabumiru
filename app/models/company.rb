f12775b9eb102ac7f57b7e4c4bd492f41170520a
EC2 Default User <ec2-user@ip-172-31-41-157.ap-northeast-1.compute.internal>
2020-05-03 05:08:28 +0000

    Companyモデルでは演算メソッドが不要

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,24 +1,5 @@
 class Company < ApplicationRecord
 
-  #メソッド：売上高営業利益率
-  def eiri_ritu
-    return  (100 * eigyou_rieki / uriagedaka).round(1)                 if eigyou_rieki.present? && uriagedaka.present?
-  end
-
-
-  #メソッド：合計利回り
-  def goukei_rimawari
-    return  (haitou_rimawari + yutai_rimawari).round(2)                if haitou_rimawari.present? && yutai_rimawari.present?
-  end
-  
-  
-  #メソッド：割安度
-  def wariyasudo
-    return  ((1- kabuka / riron_kabuka) * 100).round(1)                if kabuka.present? && riron_kabuka.present?
-  end
-
-
-
   #メソッド：1. 利回り判定
   def rimawari_hantei
     if goukei_rimawari.present?
